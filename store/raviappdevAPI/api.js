4f0c4c224c84edc1a7a7cc9346afebcc17157149
Crowdbotics <engineering@crowdbotics.com>
2023-11-20 10:02:23 +0000

    Update store.

--- store/raviappdevAPI/api.js
+++ store/raviappdevAPI/api.js
@@ -1,72 +1,52 @@
-import axios from "axios";
+import axios from "axios"
 const raviappdevAPI = axios.create({
   baseURL: "https://raviapp-dev-127751.botics.co",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json"
-  }
-});
-
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
 function api_docs_schema_retrieve(payload) {
   return raviappdevAPI.get(`/api-docs/schema/`, {
-    params: {
-      lang: payload.lang
-    }
-  });
+    params: { lang: payload.lang }
+  })
 }
-
 function api_v1_login_create(payload) {
-  return raviappdevAPI.post(`/api/v1/login/`, payload);
+  return raviappdevAPI.post(`/api/v1/login/`, payload)
 }
-
 function api_v1_signup_create(payload) {
-  return raviappdevAPI.post(`/api/v1/signup/`, payload);
+  return raviappdevAPI.post(`/api/v1/signup/`, payload)
 }
-
 function rest_auth_login_create(payload) {
-  return raviappdevAPI.post(`/rest-auth/login/`, payload);
+  return raviappdevAPI.post(`/rest-auth/login/`, payload)
 }
-
 function rest_auth_logout_create(payload) {
-  return raviappdevAPI.post(`/rest-auth/logout/`);
+  return raviappdevAPI.post(`/rest-auth/logout/`)
 }
-
 function rest_auth_password_change_create(payload) {
-  return raviappdevAPI.post(`/rest-auth/password/change/`, payload);
+  return raviappdevAPI.post(`/rest-auth/password/change/`, payload)
 }
-
 function rest_auth_password_reset_create(payload) {
-  return raviappdevAPI.post(`/rest-auth/password/reset/`, payload);
+  return raviappdevAPI.post(`/rest-auth/password/reset/`, payload)
 }
-
 function rest_auth_password_reset_confirm_create(payload) {
-  return raviappdevAPI.post(`/rest-auth/password/reset/confirm/`, payload);
+  return raviappdevAPI.post(`/rest-auth/password/reset/confirm/`, payload)
 }
-
 function rest_auth_registration_create(payload) {
-  return raviappdevAPI.post(`/rest-auth/registration/`, payload);
+  return raviappdevAPI.post(`/rest-auth/registration/`, payload)
 }
-
 function rest_auth_registration_resend_email_create(payload) {
-  return raviappdevAPI.post(`/rest-auth/registration/resend-email/`, payload);
+  return raviappdevAPI.post(`/rest-auth/registration/resend-email/`, payload)
 }
-
 function rest_auth_registration_verify_email_create(payload) {
-  return raviappdevAPI.post(`/rest-auth/registration/verify-email/`, payload);
+  return raviappdevAPI.post(`/rest-auth/registration/verify-email/`, payload)
 }
-
 function rest_auth_user_retrieve(payload) {
-  return raviappdevAPI.get(`/rest-auth/user/`);
+  return raviappdevAPI.get(`/rest-auth/user/`)
 }
-
 function rest_auth_user_update(payload) {
-  return raviappdevAPI.put(`/rest-auth/user/`, payload);
+  return raviappdevAPI.put(`/rest-auth/user/`, payload)
 }
-
 function rest_auth_user_partial_update(payload) {
-  return raviappdevAPI.patch(`/rest-auth/user/`, payload);
+  return raviappdevAPI.patch(`/rest-auth/user/`, payload)
 }
-
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
@@ -82,4 +62,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-};
+}
